6f896162288f99cd2b5f5fe02722ae1895ff501e
Vaibhav <vaibhav.2071998@gmail.com>
2022-08-27 23:19:17 +0530

    Added day5

---
+++ script/checkout.js
@@ -0,0 +1,39 @@
+let data = JSON.parse(localStorage.getItem("Data"));
+let price = +localStorage.getItem("cartPrice");
+let qty = +document.getElementById("qty").value;
+
+let tax = document.getElementById("tax");
+
+let image = document.getElementById("image");
+image.src = data.img;
+
+let stotal = price * qty;
+let taxamount = 200;
+let ftotal = stotal + taxamount;
+console.log(ftotal);
+tax.innerText = taxamount;
+let subtotal = document.getElementById("subtotal");
+subtotal.innerText = stotal;
+
+let total = document.getElementById("total");
+total.innerText = ftotal;
+
+let selectqty = () => {
+  qty = +document.getElementById("qty").value;
+  console.log(qty);
+  stotal = price * qty;
+  taxamount = 200 + 200 * (qty * 0.22);
+  ftotal = stotal + taxamount;
+  console.log(ftotal);
+
+  tax.innerText = taxamount;
+  subtotal = document.getElementById("subtotal");
+  subtotal.innerText = stotal;
+
+  total = document.getElementById("total");
+  total.innerText = ftotal;
+};
+
+let checkout = () =>{
+    window.location.href = "paymentgetway.html"
+}
