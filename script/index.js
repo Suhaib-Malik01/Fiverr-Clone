076ecdcbe89ac24af24569eaca6888cae9acf5c4
harrybhai <hariomtomar25832@gmail.com>
2022-08-27 23:53:44 +0530

    added some homepage changes and first slider changes and some footer changes

--- script/index.js
+++ script/index.js
@@ -1,6 +1,6 @@
 window.document.onkeydown = function (e) {
   if (!e) {
-    e = event;
+    e = event.preventDefault();
   }
   if (e.keyCode == 27) {
     lightbox_close();
@@ -24,7 +24,7 @@ function lightbox_close() {
 
 window.document.onkeydown = function (e) {
   if (!e) {
-    e = event;
+    e = event.preventDefault();
   }
   if (e.keyCode == 27) {
     lightbox_close1();
@@ -38,64 +38,56 @@ function lightbox_open1() {
   let lightBoxVideo = document.getElementById("VisaChipCardVideo1");
   window.scrollTo(0, 0);
   document.getElementById("light1").style.display = "block";
-  document.getElementById("this-is-big-video-slider-fade").style.display =
-    "block";
+  document.getElementById("this-is-big-video-slider-fade").style.display = "block";
   lightBoxVideo.play();
 }
 
 function lightbox_close1() {
   let lightBoxVideo = document.getElementById("VisaChipCardVideo1");
   document.getElementById("light1").style.display = "none";
-  document.getElementById("this-is-big-video-slider-fade").style.display =
-    "none";
+  document.getElementById("this-is-big-video-slider-fade").style.display = "none";
   lightBoxVideo.pause();
 }
 function lightbox_open2() {
   let lightBoxVideo = document.getElementById("VisaChipCardVideo2");
   window.scrollTo(0, 0);
   document.getElementById("light2").style.display = "block";
-  document.getElementById("this-is-big-video-slider-fade").style.display =
-    "block";
+  document.getElementById("this-is-big-video-slider-fade").style.display ="block";
   lightBoxVideo.play();
 }
 
 function lightbox_close2() {
   let lightBoxVideo = document.getElementById("VisaChipCardVideo2");
   document.getElementById("light2").style.display = "none";
-  document.getElementById("this-is-big-video-slider-fade").style.display =
-    "none";
+  document.getElementById("this-is-big-video-slider-fade").style.display ="none";
   lightBoxVideo.pause();
 }
 function lightbox_open3() {
   let lightBoxVideo = document.getElementById("VisaChipCardVideo3");
   window.scrollTo(0, 0);
   document.getElementById("light3").style.display = "block";
-  document.getElementById("this-is-big-video-slider-fade").style.display =
-    "block";
+  document.getElementById("this-is-big-video-slider-fade").style.display ="block";
   lightBoxVideo.play();
 }
 
 function lightbox_close3() {
   let lightBoxVideo = document.getElementById("VisaChipCardVideo3");
   document.getElementById("light3").style.display = "none";
-  document.getElementById("this-is-big-video-slider-fade").style.display =
-    "none";
+  document.getElementById("this-is-big-video-slider-fade").style.display ="none";
   lightBoxVideo.pause();
 }
 function lightbox_open4() {
   let lightBoxVideo = document.getElementById("VisaChipCardVideo4");
   window.scrollTo(0, 0);
   document.getElementById("light4").style.display = "block";
-  document.getElementById("this-is-big-video-slider-fade").style.display =
-    "block";
+  document.getElementById("this-is-big-video-slider-fade").style.display ="block";
   lightBoxVideo.play();
 }
 
 function lightbox_close4() {
   let lightBoxVideo = document.getElementById("VisaChipCardVideo4");
   document.getElementById("light4").style.display = "none";
-  document.getElementById("this-is-big-video-slider-fade").style.display =
-    "none";
+  document.getElementById("this-is-big-video-slider-fade").style.display ="none";
   lightBoxVideo.pause();
 }
 
@@ -129,5 +121,9 @@ function showSlides(n) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
+  dots[slideIndex - 1].className += "active";
 }
+
+
+
+
